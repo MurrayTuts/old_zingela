5dbc358efd5c8f62a598c9b25c2d707c1fc2bbef
Murray Catto <murraycatto@live.co.za>
2015-11-06 19:31:42 +0200

    add manage controller

--- zingela/db/schema.rb
+++ zingela/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106124817) do
+ActiveRecord::Schema.define(version: 20151106172709) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  limit: 255
@@ -54,6 +54,11 @@ ActiveRecord::Schema.define(version: 20151106124817) do
 
   add_index "companies", ["email"], name: "index_companies_on_email", unique: true, using: :btree
   add_index "companies", ["reset_password_token"], name: "index_companies_on_reset_password_token", unique: true, using: :btree
+
+  create_table "manages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255
