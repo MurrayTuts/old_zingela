ab810e3e8227ac99ff2724f4510c038f495e877e
Murray Catto <murraycatto@live.co.za>
2015-11-18 23:02:26 +0200

    correct user/company relationship

--- zingela/app/models/company.rb
+++ zingela/app/models/company.rb
@@ -1,7 +1,7 @@
 class Company < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :Users
+  has_many :users
   belongs_to :Admin
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
